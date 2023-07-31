4154934cad50c637b74cda570eeb0183313d16cb
Haramiday <45460677+Haramiday@users.noreply.github.com>
2023-07-31 19:20:17 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -5,9 +5,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 #import necessary libraries
 from sklearn.feature_extraction.text import CountVectorizer
-
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+# from django.views.decorators.csrf import csrf_exempt
+# from django.utils.decorators import method_decorator
 
 
 
@@ -21,8 +21,9 @@ vectorizer = CountVectorizer(max_features=50, dtype = np.float32)
 X = vectorizer.fit_transform(df["Sentence"].values).toarray()
 
 
-@method_decorator(csrf_exempt, name='post')
+# @method_decorator(csrf_exempt, name='post')
 class Prediction(APIView):
+    #authentication_classes = [SessionAuthentication, BasicAuthentication]
     def post(self, request):
         data = request.data
         value = data['sentence']
